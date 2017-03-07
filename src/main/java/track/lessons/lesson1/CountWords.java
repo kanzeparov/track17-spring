54dc39fa046c6e5c6793eeb0377ecfadbcdaac9f
kanzeparov <kanzeparov@yandex.ru>
2017-03-07 12:04:56 +0300

    Another(LAST) Version First Homework, better than the first

--- src/main/java/track/lessons/lesson1/CountWords.java
+++ src/main/java/track/lessons/lesson1/CountWords.java
@@ -32,53 +32,48 @@ public class CountWords {
      */
     public long countNumbers(File file) throws Exception {
         long sum = 0;
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            String line;
-            while ((line = reader.readLine()) != null) {
+        if (file.exists()) {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
                 try {
-                    sum += new Integer(line);
+                    sum += Long.parseLong(line);
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    continue;
                 }
             }
-            reader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
         return sum;
     }
-
 
     /**
      * Метод на вход принимает объект File, изначально результат= ""
      * Нужно пройти по всем строкам файла, и если в строка не пустая и не число
      * то надо присоединить ее к результату через пробел
+     *
      * @param file - файл с данными
      * @return - результирующая строка
      */
     public String concatWords(File file) throws Exception {
-        String sum = "";
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                try {
-                    new Integer(line);
-                    continue;
-                } catch (Exception e) {
-                    if (line.length() == 0) {
-                        continue;
+        StringBuilder sum = new StringBuilder("");
+        if (file.exists()) {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.length() > 0) {
+                    try {
+                        Long.parseLong(line);
+                    } catch (Exception e) {
+                        if (sum.length() > 0) {
+                            sum.append(" "); // Insert a space, after each word
+                        }
+                        sum.append(line);
                     }
-                    sum += line + " ";
                 }
             }
-            sum = sum.substring(0, sum.length() - 1);
-            reader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return sum;
+        String sumStr = new String(sum);
+        return sumStr;
     }
 
 }
