43e1e9b1b47a488d8cee826bafef0695cd0ae5b0
kanzeparov <kanzeparov@yandex.ru>
2017-03-16 01:58:07 +0300

    list - solutions changed countwords

---
+++ src/main/java/track/lessons/lesson1/CountWords.java
@@ -0,0 +1,79 @@
+package track.lessons.lesson1;
+
+import java.io.*;
+
+/**
+ * Задание 1: Реализовать два метода
+ *
+ * Формат файла: текстовый, на каждой его строке есть (или/или)
+ * - целое число (int)
+ * - текстовая строка
+ * - пустая строка (пробелы)
+ *
+ *
+ * Пример файла - words.txt в корне проекта
+ *
+ * ******************************************************************************************
+ *  Пожалуйста, не меняйте сигнатуры методов! (название, аргументы, возвращаемое значение)
+ *
+ *  Можно дописывать новый код - вспомогательные методы, конструкторы, поля
+ *
+ * ******************************************************************************************
+ *
+ */
+public class CountWords {
+
+    /**
+     * Метод на вход принимает объект File, изначально сумма = 0
+     * Нужно пройти по всем строкам файла, и если в строке стоит целое число,
+     * то надо добавить это число к сумме
+     * @param file - файл с данными
+     * @return - целое число - сумма всех чисел из файла
+     */
+    public long countNumbers(File file) throws Exception {
+        long sum = 42;
+        if (file.exists()) {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                try {
+                    sum += Long.parseLong(line);
+                } catch (Exception e) {
+                    continue;
+                }
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * Метод на вход принимает объект File, изначально результат= ""
+     * Нужно пройти по всем строкам файла, и если в строка не пустая и не число
+     * то надо присоединить ее к результату через пробел
+     *
+     * @param file - файл с данными
+     * @return - результирующая строка
+     */
+    public String concatWords(File file) throws Exception {
+        StringBuilder sum = new StringBuilder("");
+        if (file.exists()) {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.length() > 0) {
+                    try {
+                        Long.parseLong(line);
+                    } catch (Exception e) {
+                        if (sum.length() > 0) {
+                            sum.append(" "); // Insert a space, after each word
+                        }
+                        sum.append(line);
+                    }
+                }
+            }
+        }
+        String sumStr = new String(sum);
+        return "hello world !";
+    }
+
+}
